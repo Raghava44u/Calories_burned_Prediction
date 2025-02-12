1291609ecf9ac7b750709fcff8e77da2aca30eb2
Raghava44u <raghavadasari44@gmail.com>
2025-02-12 23:45:08 +0530

    setup finished

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup , find_packages
+from typing import List
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    Function to return a list of requirements
+    '''
+    HYPEN_E_DOT = '-e .'
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 setup(
 name="calories prediction",
 version="0.1",
@@ -9,10 +22,6 @@ description="No des",
 long_description=open("README.md").read(),
 packages=find_packages,
 url="https://github.com/Raghava44u/Calories_burned_Prediction.git",
-requires=[
-  "pandas",
-  "numpy",
-  "matplotlib",# intall dependecies here
-]
-
+install_requires=get_requirements('requirements.txt')
+ 
 )
